228258c82da046ae5b0d64e6bad9acc2401a47ab
Scott C. Reynolds <scott@scottcreynolds.com>
2013-01-01 16:05:56 -0500

    added sitcom suggestor

--- app/controllers/idea_masher_controller.rb
+++ app/controllers/idea_masher_controller.rb
@@ -1,5 +1,15 @@
 class IdeaMasherController < ApplicationController
   def show
-    @response = IdeaMasher.sitcoms.sample(1).first + " " + IdeaMasher.books.sample(1).first
+    @response = IdeaMasher.sitcoms.sample(1).first
+    @meets = IdeaMasher.other_tv.sample(1).first
+    @location = IdeaMasher.locations.sample(1).first
+    @alternate = IdeaMasher.alternate_realities.sample(1).first
+  end
+
+  def sitcom
+    sitcoms = IdeaMasher.sitcoms.sample(3)
+    @first = sitcoms.first
+    @second = sitcoms[1]
+    @third = sitcoms[2]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Onewordsuggestion::Application.routes.draw do
   get "idea_masher/show"
-
+  get '/sitcom' => "idea_masher#sitcom"
   get "sandino/index"
   match '/sandino' => "sandino#index"
   root :to => 'suggestions#index'
   get "suggestions/index"
-
-  
 end
