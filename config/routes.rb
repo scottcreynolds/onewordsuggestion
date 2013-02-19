41b9d9015a17db25e76e88ee57a4ebed673b0010
Scott C. Reynolds <scott@scottcreynolds.com>
2013-02-19 10:03:24 -0500

    routing for /warriorv

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Onewordsuggestion::Application.routes.draw do
   get "warriorv/index"
-
+  match '/warriorv' => "warriorv#index"
   get "idea_masher/show"
   get '/sitcom' => "idea_masher#sitcom"
   get "sandino/index"
