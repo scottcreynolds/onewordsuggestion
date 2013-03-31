8ff6e9733ca8412f8e9d167f3bff88ec04e18c90
Scott C. Reynolds <scott@scottcreynolds.com>
2013-03-30 23:39:56 -0400

    update suggestions warriorv

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -10,7 +10,7 @@ class Suggestion
   end
 
   def self.warriorv
-    "Constantinople, velociraptor"
+    "Constantinople, velociraptor, power tools"
   end
   def self.sandino
     "Skeleton,Window,Weed Whacker,Grandfather Clock,Mixed tape,Telephone,Shoebox,Jar of Peanut Butter,
